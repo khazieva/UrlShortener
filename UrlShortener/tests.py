008aaa2470581e77caab092212e26e794d378c80
khazieva <k.hazieva@bk.ru>
2018-11-19 14:23:26 +0300

    Add tests module.

---
+++ UrlShortener/tests.py
@@ -0,0 +1,33 @@
+import os
+from termcolor import colored
+import storage
+
+
+FAILED = colored('failed', 'red')
+PASSED = colored('passed', 'green')
+
+
+def test_init_and_remove(name, base_path):
+    storage.init_db(base_path)
+    if os.path.exists(base_path):
+        result_one = PASSED
+    else:
+        result_one = FAILED
+    storage.remove_db(base_path)
+    if os.path.exists(base_path):
+        result_two = FAILED
+    else:
+        result_two = PASSED
+    print("Test {}: init {}; remove {}.".format(name, result_one, result_two))
+
+
+def run_tests_init_and_remove():
+    test_init_and_remove("1", './my_db.sqlite')
+
+
+def run_tests():
+    run_tests_init_and_remove()
+
+
+if __name__ == "__main__":
+    run_tests()
